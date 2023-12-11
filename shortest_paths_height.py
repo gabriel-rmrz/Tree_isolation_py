05629c6321dc2961aa24a299dc245b58f410625e
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2023-12-11 13:42:38 +0100

    Adding cubical partition and updation shortest paths height

--- shortest_paths_height.py
+++ shortest_paths_height.py
@@ -18,6 +18,65 @@ def shortest_paths_height(P, cover, Hei, Base, BaseDist, inputs, Forb=None):
     For = np.zeros(numB, dtype='bool')
   
   ## Determine the shoretest paths for as many sets as possible.
+  # Paths are not computed for the sets in different components than the base
+  # sets and sets whose paths distance compared to height is to large
+  # Sort the base according to their distance:
+
+  I = np.argsort(BaseDist)
+  BaseDist = BaseDist[I]
+  Base = Base[I]
+  C = Base[0] # The current set whose patch distance is to be determined
+  Unvisited = np.zeros(numB, dtype=np.uint32) # patchees/sets not yet visited by paths
+  n = len(Base)
+  Unvisited[:n] = Base # Base set have been visited
+  UV = np.zeros(numB, dtype='bool') # Unvisited sets in logical vercor also
+  UV[Base] = True
+  a = 1 # Counts how many unvisited sets have been visited by a shortest path
+  b = n # how many unvisited sets there currently are
+  J = 1 # index of the set C (a+J-1)
+  PathLen = 1000*np.ones(numB,dtype=np.single)
+  PathNei = np.zeros(numB, dtype=np.uint32)
+  EndSet = np.zeros(numB, dtype=np.uint32)
+  PathLen[Base] = BaseDist
+  EndSet[Base] = Base
+  while a <= b:
+    nei[C] # neighbors of the set C
+    d = NeiDis[C] # the distances of the the neighbors of C
+    L = PathLen[C] + d # Path lenght to the neighbors of C
+    # Accept the path via C to N if the path lenghts are shoreter, if the 
+    # length/height ratio is small enough, if the sets are not forbidden.
+    I = L < PathLen[N] & L / Hei[N] < DHRel & ~Forb[N]
+    N = N[I]
+    if len(N)>0:
+      # Update the length, neighbor and endset for the sets N:
+      PathLen[N] = L[I]
+      PathNei[N] = C
+      EndSet[N] = EndSet[C]
+
+    # Define C as visited and N as unvisited
+    if J>1: 
+      Unvisited[a-J-1-1] = Unvisited[a-1]
+
+    a += 1
+    UV[C] = False
+    if len(N) > 0:
+      I = UV[N]
+      N = N[~I]
+      if len(N) > 0:
+        Unvisited[b+1-1: b+len(N)] = N
+        b = b + len(N)
+        UV[N] = True
+
+    # Determine the next set "C" form the unvisited sets that has the minimum
+    # path length
+    _, J = np.argmin(PathLen[Unvisited[a-1:b]])
+    C = Unvisited[a + J -1 -1)
+  
+  ## Iteratively expand trees base on the shortest paths
+  # Partition of the cover sets into search space
+
+  Partition, CC, info = cubical_partition(Ce, GD, nargout=3)
+  
 
 
 
---
+++ tools/cubical_partition.py
@@ -0,0 +1,96 @@
+'''
+ This file is part of TREEQSM.
+ 
+ TREEQSM is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ TREEQSM is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with TREEQSM.  If not, see <http://www.gnu.org/licenses/>.
+'''
+def cubical_partition(P, EL, nargout=3, NE=3):
+  '''
+  Partitions the input point cloud into cubes.
+  Inputs:
+  P             Point cloud, (n_points x 3)-matrix
+  EL            Length of the cube edges
+  NE            Number of empty edge layers
+  
+  Outputs:
+  Partition     Point cloud partitioned into cubical cells,
+                  (nx x ny x nz)-cell, where nx,ny,nz are the number
+                  of cubes in x,y,z-directions, respectively
+  CC            (n_points x 3)-matrix whose rows are the cube coordinates
+                  of each point: x,y,z-coordinates
+  Info          Minimum coordinate values, number of cubes in each
+                  coordinate direction, EL and NE.
+  Cubes         Partition contains only the non-empty cells as a 1-d array
+                and Cubes gives each cell its array-index
+  '''
+
+  if NE is None:
+    NE = 3
+
+  # The vertice of the big cube containing P
+  Min = np.min(P, axis=0)
+  Max = np.max(P, axis=0)
+
+  # Number of cubes with the edge length "EdgeLength" in the sides
+  # of the big cube
+  N = np.ceil((Max-Min)/EL)+2*NE+1
+
+  while 8*N[0]*N[1]*N[2} > 4e9:
+    EL = 1.1*EL
+    N = np.ceil((Max-Min)/EL)+2*NE+1
+
+  Info = np.concatenate([Min, N, [EL, NE]])
+  #Info = [Min, N, EL, NE]
+
+  # Calculated the cube-coordinates of the points
+  CubeCoord = np.floor((P-Min)/EL)+NE+1
+
+  # Sorts the points according a lexicographical order
+  LexOrd = CubeCoord[:,0] + N[0]* (CubeCoord[:,1] -1) + N[0]*N[1]*(CubeCoord[:,2] -1)
+  CubeCoord = CubeCoord.astype(np.uint32)
+  SortOrd = (np.argsort(LexOrd)).astype(np.uint32)
+  LexOrd = (LexOrd[SortOrd]).astype(np.uint32)
+
+  # Define "Partition"
+  Partition = {}
+  numP = len(P[:,0]) # Number of points
+  if nargout<= 3:
+    p = 1 # The index of the point under comparison
+    while p <= np:
+      t = 1
+      while (p+t <= numP) and (LexOrd[p-1] == LexOrd[p-1+t]):
+        t += 1
+      q = SortOrd[p-1]
+      Partition[tuple(CubeCoord[q,0],CubeCoord[q,1], CubeCoord[q,2])] = SortOrd[p-1:p+t-1]
+      p += t
+    return Partiotion, CubeCoord, Info
+  else:
+    numC = len(np.unique(LexOrd))
+
+    # Define "Partition"
+    Cubes = np.zeros((N[0], N[1], N[2]), dtype=np.uint32)
+
+    p = 1
+    c = 0
+    while p <= numP:
+      t = 1
+      while (p+t <= numP) and (LexOrd[p-1] == LexOrd[p-1+t]):
+        t +=1
+      q = SortOrd[p-1]
+      c += 1
+      Partition[c] = SortOrd[p-1:p+t-1]
+      Cubes[CubeCoord[q,0],CubeCoord[q,1], CubeCoord[q,2]] = c
+      p += t
+    return Partiotion, CubeCoord, Info, Cubes
+
+
